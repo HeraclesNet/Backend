2c2499c16f35e0a06e97722e5e061ba555d77162
Julio Quintero <julioquintetro@gmail.com>
2021-12-21 22:28:09 -0500

    Databese V2.0

--- src/main/resources/db/migration/postgresql/V2.0__create_database.sql
+++ src/main/resources/db/migration/postgresql/V2.0__create_database.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS "users";
-DROP TABLE IF EXISTS "files";
-DROP TABLE IF EXISTS "posts";
-DROP TABLE IF EXISTS "rutina";
+DROP TABLE IF EXISTS "users" CASCADE;
+DROP TABLE IF EXISTS "files" CASCADE;
+DROP TABLE IF EXISTS "posts" CASCADE;
+DROP TABLE IF EXISTS "rutina" CASCADE;
 
 -----------------------
 -- Table: users
@@ -29,10 +29,10 @@ CREATE TABLE "public"."users" (
 
 CREATE TABLE "public"."posts" (
 	"id" character varying(255) NOT NULL DEFAULT uuid_generate_v4()::text,
-	"user_id" character varying(255) NOT NULL,
 	"content" TEXT NOT NULL,
-	"created_at" TIMESTAMP NOT NULL,
 	"muscles" INTEGER,
+	"created_at" TIMESTAMP NOT NULL,
+	"user_id" character varying(255) NOT NULL,
 	CONSTRAINT "posts_pkey" PRIMARY KEY ("id"),
 	CONSTRAINT "fk_user_id" FOREIGN KEY ("user_id") REFERENCES "users" ("id")
 );
@@ -43,10 +43,10 @@ CREATE TABLE "public"."posts" (
 
 CREATE TABLE "public"."files" (
 	"id" character varying(255) NOT NULL DEFAULT uuid_generate_v4()::text,
+	"content" oid NOT NULL,
 	"name" character varying(255) NOT NULL,
 	"type" character varying(255) NOT NULL,
 	"post_id" character varying(255) NOT NULL,
-	"content" oid NOT NULL,
 	CONSTRAINT "files_pkey" PRIMARY KEY ("id"),
 	CONSTRAINT "fk_post_id" FOREIGN KEY ("post_id") REFERENCES "posts" ("id")
 );
